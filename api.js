6d31ea6be8c01cee9892777446801aff52913184
zackiscool000 <90630104+zackiscool000@users.noreply.github.com>
2021-10-20 09:43:12 -0400

    Create api.js

---
+++ api.js
@@ -0,0 +1,83 @@
+var LATEST_VERSION = "v3.1.72"; /* put in a separate bm-version that can be updated by php */
+
+var _api_version;
+var require = {skipDataMain: true};
+/*
+var _init = function () {
+load_script();
+}
+*/
+var _load_error = function () {
+var hostUrl = '.';
+var elem = document.getElementById('sdk-javascript');
+var styling = document.getElementById('sdk-stylesheet');
+elem.parentElement.removeChild(elem);
+styling.parentElement.removeChild(styling);
+load_script(hostUrl);
+}
+
+
+var load_script = function (hostURL) {
+var lib_name = "booster-api";
+
+var script = document.getElementById("booster-api");
+var version = script.getAttribute("data-version");
+var module = script.getAttribute("data-module");
+if (version != undefined) _api_version = version;
+if (module != undefined) lib_name = module;
+
+if (_api_version == undefined) _api_version = LATEST_VERSION;
+
+if(!(hostURL == undefined || hostURL == "")) {
+window.bb_base_path = "./shared/booster/" + _api_version;
+
+var js_script_path = "./shared/booster/" + _api_version + "/js/" + lib_name + ".js";
+var css_script_path = "./shared/booster/" + _api_version + "/css/" + lib_name + ".css";
+} else {
+window.bb_base_path = "./shared/booster/" + _api_version;
+
+var js_script_path = "./shared/booster/" + _api_version + "/js/" + lib_name + ".js";
+var css_script_path = "./shared/booster/" + _api_version + "/css/" + lib_name + ".css";
+}
+/*
+var script = document.createElement("script");
+script.setAttribute("src", js_script_path);
+//script.onerror = _load_error;
+document.head.appendChild(script, document.head.firstChild);
+*/
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.setAttribute('id', 'sdk-javascript');
+script.async = true;
+script.src = js_script_path;
+script.onerror = _load_error;
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(script, s);
+
+/*
+var stylesheet = document.createElement("link");
+stylesheet.setAttribute("rel", "stylesheet");
+stylesheet.setAttribute("type", "text/css");
+stylesheet.setAttribute("href", css_script_path);
+//stylesheet.onerror = _load_error;
+document.head.appendChild(stylesheet, document.head.firstChild);
+*/
+
+var ss = document.createElement("link");
+ss.type = "text/css";
+ss.rel = "stylesheet";
+ss.href = css_script_path;
+ss.setAttribute('id', 'sdk-stylesheet');
+document.getElementsByTagName("head")[0].appendChild(ss);
+}
+
+/*
+var version_script = document.createElement("script");
+version_script.setAttribute("src", "../bm-version.js");
+version_script.onload = _init;
+version_script.onerror = _load_error;
+document.head.appendChild(version_script, document.head.firstChild);
+*/
+
+load_script();
